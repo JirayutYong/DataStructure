53b8daeaba9ccad198384fced4f2ec05105c5ebd
JirayutYong <98368414+JirayutYong@users.noreply.github.com>
2022-01-25 11:25:01 +0700

    Delete Lab01-basic/Lab01-basic directory

--- Lab01-basic/Lab01-basic/W1mltiplication.py
+++
@@ -1,6 +0,0 @@
-print("*** multiplication or sum ***")
-num1,num2 = list(map(int, input("Enter num1 num2 : ").split()))
-if num1*num2 > 1000:
-    print("The result is",num1+num2)
-else:
-    print("The result is",num1*num2)
